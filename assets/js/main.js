d9567e481d26075d67ca001921c33a0b95923e3a
alban-okoby <albanokoby225@gmail.com>
2022-03-28 11:22:38 +0000

    Ajout de l'interactivite du menuIcon

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    $('#menuIcon').on('click', function() {
+
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+    });
+
+    $(window).on('scroll load', function() {
+        $('menuIcon').removeClass('fa-times');
+        $('header').removeClass('toggle');
+    })
+})
